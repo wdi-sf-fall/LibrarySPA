24f01e02ea7624170f818f9a8cd87c2d628855a2
iliastsangaris <iliastsangaris@gmail.com>
2014-11-13 17:19:10 -0800

    solution

--- app/assets/javascripts/books.js
+++ app/assets/javascripts/books.js
@@ -1,16 +1,36 @@
-// Namespace
+//Namespace
 var Library = {}
 
-// Fetch all books
+// Get request
 Library.loadBooks = function() { // Fetch all the books
-  console.log("Where are all the books")
+  $("#books").remove(); //clear out books
+  $.get("/books").done(function(books){ // AJAX request
+    var ul = $("<ul>").attr("id", "books"); // Create a ul to use later
+    books.forEach(function(book){ // iterate through the books our AJAX returned
+      var bookHTML = HandlebarsTemplates["books/book"](book)
+      ul.append(bookHTML)
+    })
+    $("#container").append(ul);
+  })
 }
 
-
-// Add a book
 Library.addBook = function() {
+  var title = $("#book-title-input").val(); // get title
+  $("#book-title-input").val(""); // clear title
+  var author = $("#book-author-input").val(); // get author
+  $("#book-author-input").val(""); // clear author
+  $.post('/books', {book: {title: title, author: author} }).done(function(book) {
+    Library.loadBooks();
+  }); 
 }
 
-// Delete a book
-Library.deleteBook = function() {
+//Delete a book
+Library.deleteBook = function(targetId) {
+  $.ajax({
+    type: 'delete',
+    url: '/books/' + targetId
+  }).done(function(data){
+    dis = data
+    $("#book-" + data.id).remove();
+  })
 }
